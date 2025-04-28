d45f7518c61b4dd869e4ef59f9b8ea2c27354c9a
San123san <s35102751@gmail.com>
2025-04-28 15:44:06 +0530

    sizth

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,9 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 app.use(express.json({limit: "16kb"}))   //data take when fill form in the format of json
