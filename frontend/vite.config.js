c8f098019572cccce557cc7ecfeaebaa2aae2d8a
San123san <s35102751@gmail.com>
2025-04-28 14:21:42 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
     proxy:{
-      '/api/v1/users': 'http://localhost:8000',
-      '/api/v1/expenses': 'http://localhost:8000',
     },
   },
   plugins: [react(), tailwindcss(),],
