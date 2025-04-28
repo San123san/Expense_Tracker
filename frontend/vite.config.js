f6a3615e2cb85cdfc2fbd80d0e19b47c08dc8763
San123san <s35102751@gmail.com>
2025-04-28 17:27:52 +0530

    first

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      // '/api/v1/users': 'http://localhost:8000',
-      // '/api/v1/expenses': 'http://localhost:8000',
+  // server:{
+  //   proxy:{
+  //     '/api/v1/users': 'http://localhost:8000',
+  //     '/api/v1/expenses': 'http://localhost:8000',
       
-      // '/api/v1/users': 'https://expense-tracker-1-rke4.onrender.com',
-      // '/api/v1/expenses': 'https://expense-tracker-1-rke4.onrender.com',
-    },
-  },
+  //     '/api/v1/users': 'https://expense-tracker-1-rke4.onrender.com',
+  //     '/api/v1/expenses': 'https://expense-tracker-1-rke4.onrender.com',
+  //   },
+  // },
   plugins: [react(), tailwindcss(),],
 })
